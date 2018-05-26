192d54ca99bb23a628f7b43e52f8ebd40fbd2152
wangshiqing <shiqingwang@huangbaoche.com>
2018-05-26 09:26:02 +0800

    commit changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 import 'package:myapp/src/router/application.dart';
 import 'package:myapp/src/router/routes.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:myapp/src/screen/city/main.dart';
-import 'package:myapp/src/screen/goods/main.dart';
 
 void main() => runApp(Center(child: HbcGoodsDetail()));
 
